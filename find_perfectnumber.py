e30687c79843690897dc669618dfa4271e7af28f
Bushan Bengaluru <bushan@Mac.attlocal.net>
2025-01-26 06:35:18 -0500

    01252025

--- find_perfectnumber.py
+++ find_perfectnumber.py
@@ -1,11 +1,13 @@
 #program to find perfect number
-num=int(input("Enter a number to check perfect number: ")) # input number
-sum=0 #sum of divisors
-div=1 #divisor
-while div < num: #less than to exclude number itself
-    if num % div == 0:
-        sum = sum + div
-    div = div + 1
+num=1
+while num < 1000: #less than to exclude number itself
+    div = 1
+    sum = 0 #initialize sum
+    while div < num:
+        if num % div == 0:
+            sum = sum + div
+        div = div + 1
+    num = num + 1
 if sum == num:
     print(div, "is a perfect number")
 else:
