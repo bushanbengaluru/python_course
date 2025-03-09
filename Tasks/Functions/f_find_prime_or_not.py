1b5cbded2c6ef02083d91d3949875e6fafc3492c
Bushanb <56452880+bushanbengaluru@users.noreply.github.com>
2025-03-09 22:59:54 +0000

    deco_func

--- Tasks/Functions/f_find_prime_or_not.py
+++ Tasks/Functions/f_find_prime_or_not.py
@@ -1,12 +1,20 @@
+def decorator_func(func):
+    def inner_func():
+        res = func(check_num)
+        print(res)
+        print("checking function")
+    return inner_func
+
+@decorator_func
 def find_isprime(check_num):
         for i in range(2, check_num):
             if check_num % i == 0:
                 return "Number is not prime"
-                break
-        else:
-            return "Number is prime"    
+            else:
+                return "Number is prime"    
+
 print("Program to find if given number is prime number")
 prime_nums = []
 check_num = int(input("Enter number: "))
-res = find_isprime(check_num)
-print(res)
+find_isprime()
+# res = find_isprime(check_num)
