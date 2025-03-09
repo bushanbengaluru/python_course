9c431dddbc18ab6066a927d2a991da5bf415ba24
Bushanb <56452880+bushanbengaluru@users.noreply.github.com>
2025-03-09 02:05:37 +0000

    03082025

--- Tasks/Functions/f_find_prime_or_not.py
+++ Tasks/Functions/f_find_prime_or_not.py
@@ -1,12 +1,12 @@
 def find_isprime(check_num):
-        if check_num % i == 0:
-            return(i)
+        for i in range(2, check_num):
+            if check_num % i == 0:
+                return "Number is not prime"
+                break
+        else:
+            return "Number is prime"    
 print("Program to find if given number is prime number")
 prime_nums = []
 check_num = int(input("Enter number: "))
-i = 1
-while i < check_num+1 :
-    if find_isprime(i) is True:
-        prime_nums.append(i)
-    i += 1
-print(prime_nums)
+res = find_isprime(check_num)
+print(res)
