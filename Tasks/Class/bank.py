9d7efe4c61027981a4cdfca10e50b521fafe0116
Bushan Bengaluru <bushan@Mac.attlocal.net>
2025-03-23 06:48:27 -0400

    03232025

---
+++ Tasks/Class/bank.py
@@ -0,0 +1,18 @@
+class Bank:
+    def __init__(self, name, acc_no, amount):
+        self.__name = name
+        self.__acc_no = acc_no
+        self.amount = amount
+    def info(self):
+        return self.__name, self.__acc_no, self.amount
+
+name = str(input("Enter name: "))
+acc_no = int(input("Enter account number: "))
+amount = int(input("Enter amount: "))
+b = Bank(name, acc_no, amount)
+res = b.info()
+print("{} has account number {} and amount is {}".format(res[0], res[1], res[2]))
+# Output:
+b.name="Ramesh"
+res=b.info()
+print("{} has account number {} and amount is {}".format(res[0], res[1], res[2]))
